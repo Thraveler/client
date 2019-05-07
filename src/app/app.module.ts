c4d0ae79ca19438ea2a7a56288b168f82e036883
Daniel Méndez <daphni_mgld@hotmail.com>
2019-05-07 14:51:04 -0500

    Start a new project with ionic.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MessagesService } from './services/messages.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
+    MessagesService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessagesService } from './../services/messages.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  constructor(private messageService: MessagesService) {}
+
+  ngOnInit() {
+
+    this.messageService.getMessages()
+      .subscribe(res => {
+        console.log(res);
+      });
+
+    this.messageService.getMessage('34')
+      .subscribe(res => {
+        console.log(res);
+      });
+
+    const message = {
+      content: 'Message content',
+      submittedBy: 'Daniel'
+    }
+
+    this.messageService.createMessage(message)
+      .subscribe(res => {
+        console.log(res);
+      });
+
+  }
 
 }
